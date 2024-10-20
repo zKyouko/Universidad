2530103f2826096a3aafebe5bd3adcd6ac1831e5
zKyouko <168242957+zKyouko@users.noreply.github.com>
2024-10-20 11:36:24 -0600

    clases hijas de usuario y libro

--- Biblioteca.cpp
+++ Biblioteca.cpp
@@ -29,9 +29,31 @@ public:
     string toString() const;
 };
 
-Usuario::Usuario(string id, string nombre, string email)
-    : id(id), nombre(nombre), email(email) {}
+Usuario::Usuario(string id, string nombre, string email) : id(id), nombre(nombre), email(email) {}
 
+class UsuarioEstudiante : public Usuario{
+	private:
+		string numeroMatricula;
+	public:
+		UsuarioEstudiante(string id, string nombre, string email, string numeroMatricula );
+};
+
+	UsuarioEstudiante::UsuarioEstudiante(string _id, string _nombre, string _email,string _numeroMatricula ) : Usuario(_id, _nombre, _email){
+	numeroMatricula = _numeroMatricula;
+	}
+
+	
+class UsuarioProfesor : public Usuario{
+	private:
+		string departamento;
+	public:
+		UsuarioProfesor(string id, string nombre, string email, string departamento  );
+};
+
+	UsuarioProfesor::UsuarioProfesor(string _id, string _nombre, string _email,string _departamento ) : Usuario(_id, _nombre, _email){
+	departamento  = _departamento;
+	}
+	
 class Libro {
 private:
     string id;
@@ -49,6 +71,28 @@ public:
     bool esDisponible() const;
     string toString() const;
 };
+
+class LibroFiccion : public Libro{
+	private:
+		string genero;
+	public: 
+		LibroFiccion (string id, string titulo, string autor, int anioPublicacion, string genero);
+};
+
+LibroFiccion :: LibroFiccion (string _id, string _titulo, string _autor, int _anioPublicacion, string _genero) : Libro(_id, _titulo, _autor, _anioPublicacion){
+	genero= _genero;
+}
+
+class LibroNoFiccion : public Libro{
+	private:
+		string tema ;
+	public: 
+		LibroNoFiccion (string id, string titulo, string autor, int anioPublicacion, string _tema );
+};
+
+LibroNoFiccion :: LibroNoFiccion (string _id, string _titulo, string _autor, int _anioPublicacion, string _tema ) : Libro(_id, _titulo, _autor, _anioPublicacion){
+	tema  = _tema;
+}
 
 Libro::Libro(string id, string titulo, string autor, int anioPublicacion)
     : id(id), titulo(titulo), autor(autor), anioPublicacion(anioPublicacion), disponible(true) {}
